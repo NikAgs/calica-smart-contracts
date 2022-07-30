bbf074971d91e59093d550c8ab2ad2ef295fefc6
Nik Agarwal <nikags@Niks-MacBook-Air.local>
2022-07-29 18:21:28 -0700

    Adding testing to CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: "78804413-17d5-4257-84f3-52a645e0ecca"
+  }
 };
 
 export default config;
